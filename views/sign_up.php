54119e2e71f7ecda158de79e13aef7ff58853568
elena778 <kotori122333@gmail.com>
2020-10-14 13:54:24 +0300

    form

---
+++ views/sign_up.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+</head>	
+<body>
+<form action="../controllers/sign_up.php" method="post">
+Login: <input type="text" name="login">
+Password:<input type="password" name="password">
+<input type="submit" value="Sign Up" name="log_in">
+</form>
+
+</body>
+</html>

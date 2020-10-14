4d6e3156951dde4eae35468d4ded34cfd95f1e00
elena778 <kotori122333@gmail.com>
2020-10-14 14:21:34 +0300

    22

--- controllers/sign_up.php
+++ controllers/sign_up.php
@@ -1,6 +1,21 @@
 <?
 $login=name;
 $password=pass;
-if ($login=="" || $password=="") echo "Wrong login or password";
-if ($login!==name || $password!==pass) echo "Wrong login or password";
+if ($login=="" || $password==""){
+	print("Wrong login or password");
+	redirect();
+} 
+
+if ($login!==name || $password!==pass) print("Wrong login or password");
+
+$fp=fopen('text.txt','w');
+fwrite($fp,'TEXT');
+fclose($fp);
+
+
+function redirect($url, $statusCode=303)
+{
+	header('location:', $url, true, statusCode);
+	die();
+}
 ?>
